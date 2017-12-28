4573f136f9420def1f48441082bec843536d7b3e
SunYubo <yubogo@gmail.com>
2017-12-28 14:21:41 +0800

    lesson 5 finished

--- resources/assets/js/components/register/RegisterForm.vue
+++ resources/assets/js/components/register/RegisterForm.vue
@@ -1,35 +1,46 @@
 <template>
     <form class="form-horizontal" @submit.prevent="register">
 
-        <div class="form-group">
+        <div class="form-group" :class="{'has-error' : errors.has('name')}">
             <label for="name" class="col-md-4 control-label">Name</label>
 
             <div class="col-md-6">
-                <input v-model="name" id="name" type="text" class="form-control" name="name" required autofocus>
+                <input v-model="name"
+                       v-validate="{ required: true, min: 3 }"
+                       id="name" type="text" class="form-control" name="name" required autofocus>
+                <span class="help-block" v-show="errors.has('name')">{{ errors.first('name') }}</span>
             </div>
         </div>
 
-        <div class="form-group">
+        <div class="form-group" :class="{'has-error' : errors.has('email')}">
             <label for="email" class="col-md-4 control-label">E-Mail Address</label>
 
             <div class="col-md-6">
-                <input v-model="email" id="email" type="email" class="form-control" name="email" required>
+                <input v-model="email"
+                       v-validate="{ required: true, email: true}"
+                       id="email" type="email" class="form-control" name="email" required>
+                <span class="help-block" v-show="errors.has('email')">{{ errors.first('email') }}</span>
             </div>
         </div>
 
-        <div class="form-group">
+        <div class="form-group" :class="{'has-error': errors.has('password')}">
             <label for="password" class="col-md-4 control-label">Password</label>
 
             <div class="col-md-6">
-                <input v-model="password" id="password" type="password" class="form-control" name="password" required>
+                <input v-model="password"
+                       v-validate="{required: true, min: 6}"
+                       id="password" type="password" class="form-control" name="password" required>
+                <span class="help-block" v-show="errors.has('password')">{{ errors.first('password')}}</span>
             </div>
         </div>
 
-        <div class="form-group">
+        <div class="form-group" :class="{'has-error': errors.has('password_confirmation')}">
             <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
 
             <div class="col-md-6">
-                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+                <input v-validate="{required: true, confirmed: password}"
+                        id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+                <span class="help-block" v-show="errors.has('password_confirmation')">{{ errors.first('password_confirmation')}}</span>
             </div>
         </div>
 
@@ -55,6 +66,11 @@
         },
         methods: {
             register: function () {
+                // don't register if any field validation is failed
+                if (this.errors.count() > 0) {
+                    return
+                }
+
                 let formData = {
                     name: this.name,
                     email: this.email,
